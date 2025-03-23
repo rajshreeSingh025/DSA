0d7b51021cad25794308eb683f7cc9c13de621bd
Rajshree singh <singhrajshree1001@gmail.com>
2025-03-24 00:03:06 +0530

    dupiclate NUMBER

--- q.java
+++ q.java
@@ -1,5 +1,21 @@
 public class q{
-public static void main(String[] args) {
+    public static void main(String[]args){
+        int [] num = {1 , 2 , 1 , 5 , 8 , 9};
+        System.out.println(rests(num));
+            }
+              public static boolean rests(int[] nums) {
+            
+            for(int i = 0 ; i < nums.length ; i++){
+                for(int j = i+1 ; j<nums.length ; j++){
+                    if(nums [i] == nums[j] ){
+                        return true;
+                    }
     
-}
-}
+                }
+            }
+            return false;
+        }
+        
+        }
+    
+
